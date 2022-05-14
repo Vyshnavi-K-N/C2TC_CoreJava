19f8b8250f5c5330713cb2cf96949338e4796539
user <user@DESKTOP-RM051IA>
2022-05-14 01:54:04 -0700

    first commit

---
+++ corejava/src/corejava/program1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package corejava;
+
+/**
+ * @author ${Vyshnavi K N}
+ *
+ */
+public class program1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hiee");
+
+	}
+
+}
